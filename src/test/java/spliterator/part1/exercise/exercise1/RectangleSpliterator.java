51a63039b41f128dc3649d3c67cb02c0c705a03a
Anastasiia <Anastasiia_Turenko@epam.com>
2018-01-15 01:30:55 +0300

    Rectangle spliterator

--- src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
+++ src/test/java/spliterator/part1/exercise/exercise1/RectangleSpliterator.java
@@ -1,36 +1,69 @@
 package spliterator.part1.exercise.exercise1;
 
+import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class RectangleSpliterator extends Spliterators.AbstractIntSpliterator {
 
-    public RectangleSpliterator(int[][] array) {
-        super(0, 0);
-        throw new UnsupportedOperationException();
+    private int[][] data;
+    private int startInclusive;
+    private int endExclusive;
+
+    private RectangleSpliterator(int[][] data, int startInclusive, int endExclusive) {
+        super(endExclusive - startInclusive,
+                Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.SIZED | Spliterator.NONNULL);
+        this.data = data;
+        this.startInclusive = startInclusive;
+        this.endExclusive = endExclusive;
+    }
+
+    RectangleSpliterator(int[][] data) {
+        this(data, 0, size(data));
     }
 
     @Override
     public OfInt trySplit() {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        int mid = startInclusive + (endExclusive - startInclusive) / 2;
+        int leftStart = startInclusive;
+        startInclusive += mid;
+        return new RectangleSpliterator(data, leftStart, startInclusive);
     }
 
     @Override
     public long estimateSize() {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        return endExclusive - startInclusive;
     }
 
     @Override
     public boolean tryAdvance(IntConsumer action) {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        if (startInclusive >= endExclusive) {
+            return false;
+        }
+        action.accept(at(data, startInclusive++));
+        return true;
     }
 
     @Override
     public void forEachRemaining(IntConsumer action) {
-        // TODO реализация
-        throw new UnsupportedOperationException();
+        for (int i = startInclusive; i < endExclusive; i++) {
+            action.accept(at(data, i));
+        }
+    }
+
+    private static int size(int[][] data) {
+        int size = 0;
+        for (int[] row : data) {
+            size += row.length;
+        }
+        return size;
+    }
+
+    private static int at(int[][] data, int num) {
+        int i;
+        for (i = 0; num >= data[i].length; i++) {
+            num -= data[i].length;
+        }
+        return data[i][num];
     }
 }
